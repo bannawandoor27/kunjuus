5806fc5abdf334f220a93493251831d69d13f662
Ben <113981886+bannawandoor27@users.noreply.github.com>
2023-12-29 00:31:46 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Rida Mysoon',
-    title: "Chemistry Graduate",
+    title: "Graduate Researcher",
     desciption: "A dedicated Chemistry graduate from IIT Hyderabad, I am passionate about exploring the wonders of chemistry through research and education. Beyond academics, I am engaged in sharing my love for science through teaching and continuous learning.",
     image: 'https://res.cloudinary.com/dozsrgs3w/image/upload/v1703783206/tfsc1brsngvzeybsxflx.jpg', // Replace with Rida's image link.
     resumePdf: resume
